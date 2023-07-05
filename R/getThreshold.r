3b7a16d8f66b5faad0f3b7909686325f28cfeb01
ChemoSens <caroline.peltier@inrae.fr>
2023-07-05 11:49:03 +0200

    Adding data frame (not only threshold in the output of getThreshold)

--- R/getThreshold.r
+++ R/getThreshold.r
@@ -1,4 +1,5 @@
-#' returns the threshold (=0 if the subject found the right samples for all concentrations, 1 if he/she found the right samples for all concentrations but the lightest one, etc.
+#'  @title getThreshold
+#' returns the observed sensitivity thresholds of the subjects (=0 if the subject found the right sample for all concentrations, 1 if he/she found the right sample for all concentrations but the lightest one, etc.
 #' @param res result of keepLastOccurrence (or dataframe)
 #' @inheritParams keepLastOccurence
 #' @param decreasingConcentrations vector contaning the products corresponding to the decreasing concentrations
@@ -29,29 +30,46 @@ getThreshold=function(res,decreasingConcentrations=c("C9","C8","C7","C6","C5","C
   if(!all(decreasingConcentrations%in%products)){stop("One decreasingConcentrations is not in the products")}
   subjects=levels(factor(df[,subjectName]))
   observedThreshold=rep(NA,length(subjects));names(observedThreshold)=subjects
+  dfLastSucceed=data.frame()
   for(suj in subjects)
   {
-    #print(suj)
-    dataSuj=df[df[,subjectName]==suj,resName]
-    names(dataSuj)=df[df[,subjectName]==suj,productName]
-    dataSujOrdered=dataSuj[decreasingConcentrations]
+   # print(suj)
+    dataSuj=df[df[,subjectName]==suj,]
+    rownames(dataSuj)=df[df[,subjectName]==suj,productName]
+    dataSujOrdered=dataSuj[decreasingConcentrations,]
     threshold=0
     i=1
     continue=TRUE
     while(i<length(decreasingConcentrations)+1&continue)
     {
-      if(dataSujOrdered[decreasingConcentrations[i]]=="OK")
+      if(dataSujOrdered[decreasingConcentrations[i],resName]=="OK")
       {
        # print("Succeed")
         i=i+1
       }
       else
       {
+        if(i>1)
+        {
+          infoToRemember=dataSujOrdered[decreasingConcentrations[i-1],]
+        }
+        if(i==1)
+        {
+          infoToRemember=dataSujOrdered[decreasingConcentrations[i],]
+        }
+  #      print(infoToRemember)
         continue=FALSE
       }
     }
     observedThreshold[suj]=i-1
-
+  #  print(observedThreshold)
+    if(observedThreshold[suj]==length(decreasingConcentrations))
+    {
+      infoToRemember=dataSujOrdered[decreasingConcentrations[length(decreasingConcentrations)],]
+    }
+    dfLastSucceed=rbind(dfLastSucceed,infoToRemember)
   }
-  return(length(decreasingConcentrations)-observedThreshold)
+  threshold=length(decreasingConcentrations)-observedThreshold
+  dfres=cbind(dfLastSucceed,threshold)
+  return(dfres[,c(subjectName,productName,"nClicks",resName,"threshold")])
 }
--- tests/testthat/test_getThreshold.r
+++ tests/testthat/test_getThreshold.r
@@ -2,6 +2,5 @@ data(triangular)
 df=keepLastOccurence(triangular,subjectName="Paneliste")
 seuils=getThreshold(res=df,decreasingConcentrations=c("C9","C8","C7","C6","C5","C4","C3","C2","C1"))
 
-test_that("test 1", all(expect_true(seuils==c(0,8,9))))
-
-
+test_that("test 1", expect_true(all(seuils[,"threshold"]==c(0,8,9))))
+test_that("test 2", expect_true(all(seuils[,"Paneliste"]==c("S001","S002","S003"))))
